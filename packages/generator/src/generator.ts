40182580de2c9f78e868a30d7fb434a46359f59d
Marc Jeric Espiritu <mavvystudio@gmail.com>
2023-05-07 00:15:02 +0800

    refactor: move function to helpers

--- packages/generator/src/generator.ts
+++ packages/generator/src/generator.ts
@@ -6,68 +6,11 @@ import {
 import { logger } from '@prisma/internals'
 import path from 'path'
 import { GENERATOR_NAME } from './constants'
+import { generateEnums, generateModels } from './helpers'
 // import { genEnum } from './helpers/genEnum'
 import { writeFileSafely } from './utils/writeFileSafely'
 
 const { version } = require('../package.json')
-
-export const genEnum = ({ name, values }: DMMF.DatamodelEnum) => {
-  const enumValues = values.map(({ name: n }) => n).join('\n  ')
-
-  return `
-enum ${name} {
-  ${enumValues}
-}
-`
-}
-
-const createList = (t: string, item: DMMF.Field) => {
-  if (item.isList) {
-    return `${t}[]`
-  }
-  return t
-}
-
-const createType = (item: DMMF.Field) => {
-  const t = item.type
-  if (t === 'Decimal') {
-    return 'Float'
-  }
-  if (t === 'Bytes') {
-    return 'Byte'
-  }
-  if (t === 'Unsupported') {
-    return 'String'
-  }
-  return t
-}
-
-const generateModels = (
-  models: GeneratorOptions['dmmf']['datamodel']['models'],
-) => {
-  const schema = models.reduce(
-    (prev, current) => {
-      const fields = current.fields
-        .map((item) => {
-          const itemType = createType(item)
-          return `${item.name}: ${createList(itemType, item)}`
-        })
-        .join('\n  ')
-      const data = `type ${current.name} {
-  ${fields}
-}
-
-`
-      return prev.concat(data)
-    },
-
-    '',
-  )
-  return schema
-}
-
-const generateEnums = (enums: GeneratorOptions['dmmf']['datamodel']['enums']) =>
-  enums.reduce((prev, current) => prev.concat(genEnum(current)), '')
 
 generatorHandler({
   onManifest() {
