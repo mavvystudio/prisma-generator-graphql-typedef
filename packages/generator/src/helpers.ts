1f7d939d08c888030937dfcbe79a0c8e21bf4fd0
Marc Jeric Espiritu <mavvystudio@gmail.com>
2024-05-29 23:37:25 +0800

    feat: add mandatory fields

--- packages/generator/src/helpers.ts
+++ packages/generator/src/helpers.ts
@@ -1,5 +1,17 @@
 import { DMMF, GeneratorOptions } from '@prisma/generator-helper'
 
+type ItemType = {
+  dataType: string | undefined
+  gqlAttrsObj: GqlAttrsObject
+  isRequired: boolean
+}
+
+type GqlAttrsObject = {
+  ['@gqlIgnore']?: boolean
+  ['@gqlType']?: string
+  ['@gqlNonNullElement']?: boolean
+}
+
 export const genEnum = ({ name, values }: DMMF.DatamodelEnum) => {
   const enumValues = values.map(({ name: n }) => n).join('\n  ')
 
@@ -10,16 +22,13 @@ enum ${name} {
 `
 }
 
-const createList = (t: string, item: DMMF.Field) => {
+const createList = (itemType: ItemType, item: DMMF.Field) => {
+  const requiredSuffix = itemType.isRequired ? '!' : ''
+  const nonNullSuffix = itemType.gqlAttrsObj['@gqlNonNullElement'] ? '!' : ''
   if (item.isList) {
-    return `[${t}]`
+    return `[${itemType.dataType}${nonNullSuffix}]${requiredSuffix}`
   }
-  return t
-}
-
-type GqlAttrsObject = {
-  ['@gqlIgnore']?: boolean
-  ['@gqlType']?: string
+  return `${itemType.dataType}${requiredSuffix}`
 }
 
 const getGqlAttrs = (item: DMMF.Field) => {
@@ -27,32 +36,49 @@ const getGqlAttrs = (item: DMMF.Field) => {
   if (!attrs) {
     return {} as GqlAttrsObject
   }
+  const isNonNullElement = item.documentation?.includes('@gqlNonNullElement')
   return {
     [attrs[0]]: attrs[1] || true,
+    ['@gqlNonNullElement']: isNonNullElement,
   }
+}
+
+const createDataType = (data: string) => {
+  if (data === 'Decimal') {
+    return 'Float'
+  }
+  if (data === 'Bytes') {
+    return 'Byte'
+  }
+  if (data === 'Unsupported') {
+    return 'String'
+  }
+  return data
 }
 
 const createType = (item: DMMF.Field) => {
   const gqlAttrsObj = getGqlAttrs(item)
+  const { isRequired } = item
 
   if (gqlAttrsObj['@gqlIgnore']) {
     return null
   }
   if (gqlAttrsObj['@gqlType']) {
-    return gqlAttrsObj['@gqlType']
+    return {
+      dataType: gqlAttrsObj['@gqlType'],
+      gqlAttrsObj,
+      isRequired,
+    }
   }
   const t = item.type
 
-  if (t === 'Decimal') {
-    return 'Float'
+  const dataType = createDataType(t)
+
+  return {
+    dataType,
+    gqlAttrsObj,
+    isRequired,
   }
-  if (t === 'Bytes') {
-    return 'Byte'
-  }
-  if (t === 'Unsupported') {
-    return 'String'
-  }
-  return t
 }
 
 export const generateModels = (
